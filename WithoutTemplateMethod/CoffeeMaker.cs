5cb9f8bf28cf6b03c8542d37a1f421cc49a66ea7
TimIntim <122295758+CopyPasteDev@users.noreply.github.com>
2023-02-18 20:53:43 +0500

    Добавил пример кода с дублированием

---
+++ WithoutTemplateMethod/CoffeeMaker.cs
@@ -0,0 +1,32 @@
+﻿namespace WithoutTemplateMethod;
+
+public class CoffeeMaker
+{
+    public void MakeCoffee()
+    {
+        BoilWater();
+        GrindCoffee();
+        BrewCoffee();
+        PourIntoCup();
+    }
+
+    private void BoilWater()
+    {
+        Console.WriteLine("Boiling water");
+    }
+
+    private void GrindCoffee()
+    {
+        Console.WriteLine("Grinding coffee beans");
+    }
+
+    private void BrewCoffee()
+    {
+        Console.WriteLine("Brewing coffee");
+    }
+
+    private void PourIntoCup()
+    {
+        Console.WriteLine("Pouring coffee into cup");
+    }
+}
---
+++ WithoutTemplateMethod/FrenchPressCoffeeMaker.cs
@@ -0,0 +1,50 @@
+﻿namespace WithoutTemplateMethod;
+
+public class FrenchPressCoffeeMaker
+{
+    public void MakeCoffee()
+    {
+        BoilWater();
+        GrindCoffee();
+        SteepCoffee();
+        PourIntoCup();
+        AddMilk();
+        AddSugar();
+    }
+
+    // The method are the same as in CoffeeMaker.cs
+    private void BoilWater()
+    {
+        Console.WriteLine("Boiling water");
+    }
+
+    // The method are the same as in CoffeeMaker.cs
+    private void GrindCoffee()
+    {
+        Console.WriteLine("Grinding coffee beans");
+    }
+
+    // The method aren't in CoffeeMaker.cs
+    private void SteepCoffee()
+    {
+        Console.WriteLine("Steeping coffee in French press");
+    }
+
+    // The method are the same as in CoffeeMaker.cs
+    private void PourIntoCup()
+    {
+        Console.WriteLine("Pouring coffee into cup");
+    }
+
+    // The method aren't in CoffeeMaker.cs
+    private void AddMilk()
+    {
+        Console.WriteLine("Adding milk to taste");
+    }
+
+    // The method aren't in CoffeeMaker.cs
+    private void AddSugar()
+    {
+        Console.WriteLine("Adding sugar to taste");
+    }
+}
